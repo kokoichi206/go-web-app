7165491f6406920f5bf6c6bdf1b36f278e8d7140
kokoichi <tt.300607@gmail.com>
2021-07-06 00:27:24 +0900

    chap1: websocket

--- chat/main.go
+++ chat/main.go
@@ -4,27 +4,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"sync"
-
-	"example.com/trace"
 )
 
 type templateHandler struct {
-	once sync.Once
-	filename	string
-	templ	*template.Template
+	once     sync.Once
+	filename string
+	templ    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))})
+	t.once.Do(func() { t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename))) })
 	t.templ.Execute(w, nil)
 }
 
 func main() {
 	r := newRoom()
-	r.tracer = trace.New(os.Stdout)
+	// r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
